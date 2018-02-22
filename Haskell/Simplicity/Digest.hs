5a19a61a89953c002d2f23952492cac941cb613f
Russell O'Connor <roconnor@blockstream.io>
2018-02-22 16:12:30 -0500

    Revamp Digest module.
    We need Hash256 to have a concrete representation for sharing purposes when computing Merkle roots.

--- Haskell/Simplicity/Digest.hs
+++ Haskell/Simplicity/Digest.hs
@@ -1,8 +1,8 @@
 -- | This modules wraps Data.Digest.Pure.SHA in order to simulate direct access to the SHA-256 compression function by providing access to SHA-256 midstates.
 module Simplicity.Digest
-  (Hash256, hash256, integerHash256, bsHash
-  ,Block
-  ,compress, compressHalf
+  (Hash256(..), integerHash256
+  ,IV, bsIv, ivHash, bsHash
+  ,Block ,compress, compressHalf
   ) where
 
 import Data.Binary.Get (Decoder(..), pushChunk, pushChunks, pushEndOfInput)
@@ -13,13 +13,7 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 
 -- | Represents a 256-bit hash value or midstate from SHA-256.
-newtype Hash256 = Hash256 (Decoder SHA256State)
-
--- | Extracts the 256 hash value as an 'BS.ByteString'.
-hash256 :: Hash256 -> BS.ByteString
-hash256 (Hash256 state) = case pushEndOfInput state of
-  Done _ _ x -> BSL.toStrict (encode x)
-  _          -> error "getHash256 unexpected decoder state"
+newtype Hash256 = Hash256 { hash256 :: BS.ByteString }
 
 -- | Extracts the 256 hash value as an integer.
 integerHash256 :: Hash256 -> Integer
@@ -27,18 +21,30 @@ integerHash256 h = BS.foldl' go 0 $ hash256 h
  where
   go n w = (n `shiftL` 8) .|. fromIntegral w
 
+newtype IV = IV (Decoder SHA256State)
+
+-- | Computes a SHA-256 hash from a lazy 'BSL.ByteString' representing an initial value.
+bsIv :: BSL.ByteString -> IV
+bsIv = IV . pushChunks sha256Incremental . padSHA1
+
+-- | Realize a initial value as a concrete Hash.
+ivHash :: IV -> Hash256
+ivHash (IV state) =  case pushEndOfInput state of
+  Done _ _ x -> Hash256 . BSL.toStrict . encode $ x
+  _          -> error "getHash256 unexpected decoder state"
+
 -- | Computes a SHA-256 hash from a lazy 'BSL.ByteString'.
 bsHash :: BSL.ByteString -> Hash256
-bsHash = Hash256 . pushChunks sha256Incremental . padSHA1
+bsHash = ivHash .bsIv
 
 type Block = (Hash256, Hash256)
 
 -- | Given an initial value and a block of data consisting of a pair of hashes, apply the SHA-256 compression function.
-compress :: Hash256 -> Block -> Hash256
-compress (Hash256 iv) (h1, h2) = Hash256 $ iv `pushChunk` hash256 h1 `pushChunk` hash256 h2
+compress :: IV -> Block -> IV
+compress (IV state) (h1, h2) = IV $ state `pushChunk` hash256 h1 `pushChunk` hash256 h2
 
 -- | Given an initial value and a block of data consisting of a one hash followed by 256-bits of zeros, apply the SHA-256 compression function.
-compressHalf :: Hash256 -> Hash256 -> Hash256
-compressHalf (Hash256 iv) h = Hash256 $ iv `pushChunk` hash256 h `pushChunk` zero
+compressHalf :: IV -> Hash256 -> IV
+compressHalf iv h = compress iv (h, zero)
  where
-  zero = BS.replicate 32 0
+  zero = Hash256 (BS.replicate 32 0)
