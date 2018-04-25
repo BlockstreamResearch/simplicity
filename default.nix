38ffcfba00967ce426331435973c828fad456bf9
Russell O'Connor <roconnor@blockstream.io>
2018-04-25 12:53:43 -0400

    Make the version of Coq used configurable.
    This is a bit quick and dirty.  Adding our project to the parameterized coqPackages attribute would be more robust.

--- default.nix
+++ default.nix
@@ -1,8 +1,11 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }: rec
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822", coqVersion ? "coq" }: rec
 {
   haskell = nixpkgs.haskell.packages.${compiler}.callPackage ./Simplicity.Haskell.nix { };
   coq = nixpkgs.callPackage ./Simplicity.Coq.nix {
     inherit vst;
+    coq = nixpkgs.${coqVersion};
   };
-  vst = nixpkgs.callPackage ./vst.nix { };
+  vst = nixpkgs.callPackage ./vst.nix {
+    coq = nixpkgs.${coqVersion};
+  };
 }
