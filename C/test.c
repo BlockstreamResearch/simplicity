c5993e8a0c328c368e4e1e5ac14079ebfc8e6619
Russell O'Connor <roconnor@blockstream.io>
2019-03-07 00:04:22 -0500

    Refactor 'eval.c' into 'eval.c' and 'frame.c'
    This refactoring will help prevent a header inclusion cycle from forming when jets are introduced.
    ('eval.h' depends on 'dag.h' which will depend on jets, which will depend on 'frame.h')
    
    We also generalize 'read32' and 'write32' to operate on 8, 16, 32, and 64 bits.
    We replace 'read256' and 'write256' with a more general 'read32s' and 'write32s' functions operating on arrays.

--- C/test.c
+++ C/test.c
@@ -101,12 +101,11 @@ static void test_hashBlock(void) {
       UWORD input[roundUWord(256+512)];
       { frameItem frame = initWriteFrame(256+512, &input[roundUWord(256+512)]);
         /* Set SHA-256's initial value. */
-        write256(&frame, (sha256_midstate){
-          .s = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 }
-        });
+        write32s(&frame, (uint32_t[8])
+            { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 }
+          , 8);
         /* Set the block to be compressed to "abc" with padding. */
-        write256(&frame, (sha256_midstate){ .s = { 0x61626380 } });
-        write256(&frame, (sha256_midstate){ .s = { [7] = 0x18 } });
+        write32s(&frame, (uint32_t[16]){ [0] = 0x61626380, [15] = 0x18 }, 16);
       }
       computeEvalTCOBounds(analysis, dag, type_dag, (size_t)len);
       if (evalTCOExpression(output, 256, input, 256+512, analysis, dag, type_dag, (size_t)len)) {
@@ -114,8 +113,9 @@ static void test_hashBlock(void) {
         const uint32_t expectedHash[8] = { 0xba7816bful, 0x8f01cfeaul, 0x414140deul, 0x5dae2223ul
                                          , 0xb00361a3ul, 0x96177a9cul, 0xb410ff61ul, 0xf20015adul };
         frameItem frame = initReadFrame(256, &output[0]);
-        sha256_midstate result = read256(&frame);
-        if (0 == memcmp(expectedHash, result.s, sizeof(uint32_t[8]))) {
+        uint32_t result[8];
+        read32s(result, 8, &frame);
+        if (0 == memcmp(expectedHash, result, sizeof(uint32_t[8]))) {
           successes++;
         } else {
           failures++;
@@ -158,16 +158,12 @@ static void test_schnorrAssert(void) {
       computeEvalTCOBounds(analysis, dag, type_dag, (size_t)len);
       { frameItem frame = initWriteFrame(1+256+256+512, &input[roundUWord(1+256+256+512)]);
         writeBit(&frame, 0);
-        write256(&frame, (sha256_midstate){
-          .s = { 0x79BE667E, 0xF9DCBBAC, 0x55A06295, 0xCE870B07, 0x029BFCDB, 0x2DCE28D9, 0x59F2815B, 0x16F81798 }
-        });
-        write256(&frame, (sha256_midstate){0});
-        write256(&frame, (sha256_midstate){
-          .s = { 0x787A848E, 0x71043D28, 0x0C50470E, 0x8E1532B2, 0xDD5D20EE, 0x912A45DB, 0xDD2BD1DF, 0xBF187EF6 }
-        });
-        write256(&frame, (sha256_midstate){
-          .s = { 0x7031A988, 0x31859DC3, 0x4DFFEEDD, 0xA8683184, 0x2CCD0079, 0xE1F92AF1, 0x77F7F22C, 0xC1DCED05 }
-        });
+        write32s(&frame, (uint32_t[32])
+            { 0x79BE667E, 0xF9DCBBAC, 0x55A06295, 0xCE870B07, 0x029BFCDB, 0x2DCE28D9, 0x59F2815B, 0x16F81798
+            , 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000
+            , 0x787A848E, 0x71043D28, 0x0C50470E, 0x8E1532B2, 0xDD5D20EE, 0x912A45DB, 0xDD2BD1DF, 0xBF187EF6
+            , 0x7031A988, 0x31859DC3, 0x4DFFEEDD, 0xA8683184, 0x2CCD0079, 0xE1F92AF1, 0x77F7F22C, 0xC1DCED05 }
+          , 32);
       }
       if (evalTCOExpression(NULL, 0, input, 1+256+256+512, analysis, dag, type_dag, (size_t)len)) {
         successes++;
@@ -178,18 +174,12 @@ static void test_schnorrAssert(void) {
 
       { frameItem frame = initWriteFrame(1+256+256+512, &input[roundUWord(1+256+256+512)]);
         writeBit(&frame, 1);
-        write256(&frame, (sha256_midstate){
-          .s = { 0xFAC2114C, 0x2FBB0915, 0x27EB7C64, 0xECB11F80, 0x21CB45E8, 0xE7809D3C, 0x0938E4B8, 0xC0E5F84B }
-        });
-        write256(&frame, (sha256_midstate){
-          .s = { 0x5E2D58D8, 0xB3BCDF1A, 0xBADEC782, 0x9054F90D, 0xDA9805AA, 0xB56C7733, 0x3024B9D0, 0xA508B75C }
-        });
-        write256(&frame, (sha256_midstate){
-          .s = { 0x00DA9B08, 0x172A9B6F, 0x0466A2DE, 0xFD817F2D, 0x7AB437E0, 0xD253CB53, 0x95A96386, 0x6B3574BE }
-        });
-        write256(&frame, (sha256_midstate){
-          .s = { 0xD092F9D8, 0x60F1776A, 0x1F7412AD, 0x8A1EB50D, 0xACCC222B, 0xC8C0E26B, 0x2056DF2F, 0x273EFDEC }
-        });
+        write32s(&frame, (uint32_t[32])
+            { 0xFAC2114C, 0x2FBB0915, 0x27EB7C64, 0xECB11F80, 0x21CB45E8, 0xE7809D3C, 0x0938E4B8, 0xC0E5F84B
+            , 0x5E2D58D8, 0xB3BCDF1A, 0xBADEC782, 0x9054F90D, 0xDA9805AA, 0xB56C7733, 0x3024B9D0, 0xA508B75C
+            , 0x00DA9B08, 0x172A9B6F, 0x0466A2DE, 0xFD817F2D, 0x7AB437E0, 0xD253CB53, 0x95A96386, 0x6B3574BE
+            , 0xD092F9D8, 0x60F1776A, 0x1F7412AD, 0x8A1EB50D, 0xACCC222B, 0xC8C0E26B, 0x2056DF2F, 0x273EFDEC }
+          , 32);
       }
       if (!evalTCOExpression(NULL, 0, input, 1+256+256+512, analysis, dag, type_dag, (size_t)len)) {
         successes++;
