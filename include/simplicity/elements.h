b7c97f75374e8e495cbf083de0f07cbeb17f2582
Russell O'Connor <roconnor@blockstream.io>
2019-06-25 12:46:43 -0400

    Add a function to the Elements Simplicity API to deserialize and evaluate a Simplicity program.

--- include/simplicity/elements.h
+++ include/simplicity/elements.h
@@ -88,4 +88,24 @@ typedef struct transaction transaction;
  * Precondition: NULL != rawTx
  */
 extern transaction* elements_simplicity_mallocTransaction(const rawTransaction* rawTx);
+
+/* Deserialize a Simplicity program from 'file' and execute it in the environment of the 'ix'th input of 'tx'.
+ * If the file isn't a proper encoding of a Simplicity program, '*success' is set to false.
+ * If EOF isn't encountered at the end of decoding, '*success' is set to false.
+ * If 'cmr != NULL' and the commitment Merkle root of the decoded expression doesn't match 'cmr' then '*success' is set to false.
+ * If 'wmr != NULL' and the witness Merkle root of the decoded expression doesn't match 'wmr' then '*success' is set to false.
+ * Otherwise evaluation proceeds and '*success' is set to the result of evaluation.
+ *
+ * If at any time there is a transient error, such as malloc failing or an I/O error reading from 'file'
+ * then 'false' is returned, and 'success' and 'file' may be modified.
+ * Otherwise, 'true' is returned.
+ *
+ * Precondition: NULL != success;
+ *               NULL != tx;
+ *               NULL != cmr implies uint32_t cmr[8]
+ *               NULL != wmr implies uint32_t wmr[8]
+ *               NULL != file;
+ */
+extern bool elements_simplicity_execSimplicity(bool* success, const transaction* tx, uint_fast32_t ix,
+                                               const uint32_t* cmr, const uint32_t* wmr, FILE* file);
 #endif
