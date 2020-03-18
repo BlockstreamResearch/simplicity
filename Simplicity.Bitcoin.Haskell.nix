41076667203ec0180d1c73312564c20c5210a649
Russell O'Connor <roconnor@blockstream.io>
2020-03-18 13:22:49 -0400

    Rename .cabal files as .cabal.delete-this-extension. Most Haskell tools choke when more than one .cabal file exists in a directory. We cannot so easily put each .cabal file in its own directory because their source code is shared. Instead we will request that the user rename (or copy or symlink) the .cabal file they wish to build.
    nix-shell hooks are added to automatically symlink the requested .cabal file as `Simplicity.cabal`.
    (Notice that the file name for the .cabal file isn't relevent to the build).

--- Simplicity.Bitcoin.Haskell.nix
+++ Simplicity.Bitcoin.Haskell.nix
@@ -3,11 +3,15 @@ mkDerivation (rec {
   pname = "Simplicity-Bitcoin";
   version = "0.0.0";
   src = lib.sourceFilesBySuffices
-      (lib.sourceByRegex ./. ["^LICENSE$" "^Simplicity-Bitcoin\.cabal$" "^Setup.hs$" "^Tests.hs$" "^Haskell$" "^Haskell/.*"])
-    ["LICENSE" ".cabal" ".hs"];
+      (lib.sourceByRegex ./. ["^LICENSE$" "^Simplicity-Bitcoin\.cabal.delete-this-extension$" "^Setup.hs$" "^Tests.hs$" "^Haskell$" "^Haskell/.*"])
+    ["LICENSE" ".cabal.delete-this-extension" ".hs"];
   libraryHaskellDepends = [ base binary cereal lens-family MemoTrie mtl SHA split unification-fd vector ];
   testHaskellDepends = libraryHaskellDepends ++ [ QuickCheck tasty tasty-hunit tasty-quickcheck ];
   testTarget = ''--test-option="--quickcheck-replay=582534"'';
+
+  prePatch = "mv ${pname}.cabal.delete-this-extension ${pname}.cabal";
+
+  shellHook = "[ -f ${pname}.cabal.delete-this-extension ] && ln -sfT ${pname}.cabal.delete-this-extension Simplicity.cabal";
 
   license = stdenv.lib.licenses.mit;
 })
--- Simplicity.Haskell.nix
+++ Simplicity.Haskell.nix
@@ -3,11 +3,15 @@ mkDerivation (rec {
   pname = "Simplicity";
   version = "0.0.0";
   src = lib.sourceFilesBySuffices
-      (lib.sourceByRegex ./. ["^LICENSE$" "^Simplicity\.cabal$" "^Setup.hs$" "^Tests.hs$" "^Haskell$" "^Haskell/.*"])
-    ["LICENSE" ".cabal" ".hs" ".hsig"];
+      (lib.sourceByRegex ./. ["^LICENSE$" "^Simplicity\.cabal.delete-this-extension$" "^Setup.hs$" "^Tests.hs$" "^Haskell$" "^Haskell/.*"])
+    ["LICENSE" ".cabal.delete-this-extension" ".hs" ".hsig"];
   libraryHaskellDepends = [ base binary cereal lens-family MemoTrie mtl SHA split unification-fd vector ];
   testHaskellDepends = libraryHaskellDepends ++ [ QuickCheck tasty tasty-hunit tasty-quickcheck ];
   testTarget = ''--test-option="--quickcheck-replay=582534"'';
+
+  prePatch = "mv ${pname}.cabal.delete-this-extension ${pname}.cabal";
+
+  shellHook = "[ -f ${pname}.cabal.delete-this-extension ] && ln -sfT ${pname}.cabal.delete-this-extension Simplicity.cabal";
 
   # Cabal's haddock doesn't work for Backpack / internal libraries / modules reexports.
   # Until that is fix we manually generate some documentation pages
