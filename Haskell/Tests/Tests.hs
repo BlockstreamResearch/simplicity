3ff9ebe809d3cf42fd72653c5887d955b6f703f5
Russell O'Connor <roconnor@blockstream.io>
2021-06-02 15:09:43 -0400

    Add Tests for secp256k1 jets.

--- Haskell/Tests/Tests.hs
+++ Haskell/Tests/Tests.hs
@@ -5,6 +5,7 @@ import Test.Tasty
 
 import qualified Simplicity.BitMachine.Tests as BitMachine
 import qualified Simplicity.BitMachine.StaticAnalysis.Tests as StaticAnalysis
+import qualified Simplicity.FFI.Tests as FFI
 import qualified Simplicity.Programs.Tests as Programs
 import qualified Simplicity.Bitcoin.Serialization.Tests as Serialization
 import qualified Simplicity.Elements.Tests as Elements
@@ -16,6 +17,7 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests"
       [ Programs.tests
+      , FFI.tests
       , BitMachine.tests
       , StaticAnalysis.tests
       , Serialization.tests
