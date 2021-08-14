017d87a62211741334c598e427279fbffc25c1f7
Russell O'Connor <roconnor@blockstream.io>
2021-08-14 12:56:21 -0400

    Replace mergePkgs with nixpkgs.mkShell.

--- shell.nix
+++ shell.nix
@@ -6,28 +6,16 @@
 let
   simplicity      = import ./. {};
   optional        = nixpkgs.lib.optional;
-  mkDerivation    = nixpkgs.stdenv.mkDerivation;
+  haskellDevTools = pkgs: with pkgs; [cabal-install hlint hasktags];
+  haskellPkgs     = pkgs: simplicity.haskell.buildInputs ++ haskellDevTools pkgs;
+  haskellDevEnv   = simplicity.haskellPackages.ghcWithPackages haskellPkgs;
 
-  haskellDrv =
-    let
-      haskellDevTools = pkgs: with pkgs; [cabal-install hlint hasktags];
-      haskellPkgs     = pkgs: simplicity.haskell.buildInputs ++ haskellDevTools pkgs;
-      haskellDevEnv   = simplicity.haskellPackages.ghcWithPackages haskellPkgs;
-    in
-      mkDerivation {
-        name = "haskell-dev-env";
-        buildInputs = [haskellDevEnv];
-      };
-
-  mergePkgs = name: drvs: mkDerivation {
-    inherit name;
-    buildInputs           = builtins.concatMap (p: p.buildInputs) drvs;
-    nativeBuildInputs     = builtins.concatMap (p: p.nativeBuildInputs) drvs;
-    propagatedBuildInputs = builtins.concatMap (p: p.propagatedBuildInputs) drvs;
-  };
-
-  selectedPkgs = optional haskell haskellDrv
+  selectedPkgs = optional false false # haskell simplicity.haskell # haskellDrv
               ++ optional coq     simplicity.coq
               ++ optional c       simplicity.c;
 in
-  mergePkgs "simplicity-dev-env" selectedPkgs
+  nixpkgs.mkShell {
+    packages = optional haskell haskellDevEnv;
+    inputsFrom = optional coq     simplicity.coq
+              ++ optional c       simplicity.c;
+  }
