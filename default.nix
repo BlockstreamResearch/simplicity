2b7391d6264e4965d1f6cb577f92cd23afed46e8
Russell O'Connor <roconnor@blockstream.io>
2022-05-26 14:13:07 -0400

    Update to Coq 8.14.

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8107", coqVersion ? "coqPackages_8_13", secp256k1git ? null}:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8107", coqVersion ? "coqPackages_8_14", secp256k1git ? null}:
 let hp = nixpkgs.haskell.packages.${compiler};
  in rec
 {
