25fe985482bc76ed7ff6498ae0c5962885dbc13b
Andrew Poelstra <apoelstra@wpsoftware.net>
2022-08-26 16:04:17 +0000

    add GenPrimitives to cabal file
    This allows you to just do `cabal run GenPrimitives` and then `mv *.inc ./C/primitive/elements/`

--- Simplicity.Haskell.nix
+++ Simplicity.Haskell.nix
@@ -4,6 +4,7 @@ mkDerivation (rec {
   version = "0.0.0";
   src = lib.sourceFilesBySuffices
       (lib.sourceByRegex ./. ["^LICENSE$" "^Simplicity\.cabal$" "^Setup.hs$" "^Tests.hs$" "^Haskell$" "^Haskell/.*"
+                              "^Haskell-GenerateC$" "^Haskell-GenerateC/.*"
                               "^C$" "^C/uword.h" "^C/bitstring.h" "^C/frame.*" "^C/jets.*" "^C/sha256.*" "^C/unreachable.h"
                               "^C/jets-secp256k1.c$" "^C/secp256k1$" "^C/secp256k1/.*"
                               "^C/include$" "^C/include/simplicity$" "^C/include/simplicity/elements$" "^C/include/simplicity/elements/env.h"
