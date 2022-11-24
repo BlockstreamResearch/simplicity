3ff3d52f6e6618f97a707f9a365be07de216fdbd
Russell O'Connor <roconnor@blockstream.io>
2022-11-24 15:38:34 -0500

    Use cabal haddock to generate documentation.

--- Simplicity.Haskell.nix
+++ Simplicity.Haskell.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, binary, cereal, lens-family, lib, MemoTrie, mtl, QuickCheck, stdenv, split, tasty, tasty-hunit, tasty-quickcheck, tardis, unification-fd, vector }:
+{ mkDerivation, cabal-install, base, binary, cereal, lens-family, lib, MemoTrie, mtl, QuickCheck, stdenv, split, tasty, tasty-hunit, tasty-quickcheck, tardis, unification-fd, vector }:
 mkDerivation (rec {
   pname = "Simplicity";
   version = "0.0.0";
@@ -23,11 +23,23 @@ mkDerivation (rec {
   # testFlags = ["--quickcheck-replay=582534"];
 
   # Cabal's haddock doesn't work for Backpack / internal libraries / modules reexports.
-  # Until that is fix we manually generate some documentation pages
-  haddockFlags = ["--haddock-option='--use-contents=index.html'"];
+  # Under ghc8, the documenation generated is pretty badly broken.
+  # Under ghc9, Setup.hs haddock simply crashes.
+  #
+  # To work around these issues we manually build the documentation using cabal haddock.
+  # Even cabal haddock hardcodes links to internal modules that are reexported which is a problem.
+  # To deal with that, we build directly into the $doc directly.
+  # This isn't very great because the documenation isn't really in the right place, but the result is usable.
+  doHaddock = false;
+  enableSeparateDocOutput = true;
+  buildTools = [ cabal-install ];
+  postHaddock = ''
+   CABAL_CONFIG=/dev/null CABAL_DIR=`pwd` env -u GHC_PACKAGE_PATH cabal --package-db=$packageConfDir configure --docdir=$doc/share/doc/${pname}-${version}
+   CABAL_CONFIG=/dev/null CABAL_DIR=`pwd` env -u GHC_PACKAGE_PATH cabal --package-db=$packageConfDir haddock --distdir=$doc
+  '';
+  # Delete all cabal build material other than the documentation.
   postInstall = ''
-    cp ${./manual-index.html} $doc/share/doc/${pname}-${version}/html/index.html
-    cp ${./Simplicity-Primitive.html} $doc/share/doc/${pname}-${version}/html/Simplicity-Primitive.html
+    find $doc ! -path '*/doc/*' -delete ||:
   '';
 
   license = lib.licenses.mit;
