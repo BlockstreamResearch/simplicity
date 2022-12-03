63ab1c89f3faf20bf80ccee58c60372c10cec556
Christian Lewe <ch_lewe@agdsn.me>
2022-12-03 21:05:34 +0100

    Update zero

--- Coq/Simplicity/SHA256.v
+++ Coq/Simplicity/SHA256.v
@@ -457,7 +457,7 @@ Hint Immediate hashBlock_Parametric : parametricity.
 Require Import Simplicity.MerkleRoot.
 
 Fact identityRoot_hashBlock : map Byte.unsigned (hash256_to_bytelist (identityRoot hashBlock)) =
-  map Byte.unsigned (sha.functional_prog.hexstring_to_bytelist "a123e1c054e91dd8ebc691666621dcb6cd91618e49d2a06d8fca709a0d706952").
+  map Byte.unsigned (sha.functional_prog.hexstring_to_bytelist "86e432d402d443369c4cf34d18e5854af8858295737fc935c53a995cd9629f40").
 Proof.
 vm_compute.
 reflexivity.
