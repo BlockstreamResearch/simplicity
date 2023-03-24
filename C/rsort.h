1e5942261bbb1fec7fcc45570ceb8ffa4b7714bd
Russell O'Connor <roconnor@blockstream.io>
2023-03-24 08:14:32 -0400

    Update documentation for hasDuplicates

--- C/rsort.h
+++ C/rsort.h
@@ -14,14 +14,12 @@ _Static_assert(UCHAR_MAX < SIZE_MAX, "UCHAR_MAX >= SIZE_MAX");
 /* Internal function used by 'hasDuplicates'.  Do not call directly. */
 const sha256_midstate* rsort(size_t* scratch, const sha256_midstate** a, size_t len, size_t level);
 
-/* Searches for duplicates in an array of pointers to 'sha256_midstate's.
+/* Searches for duplicates in an array of 'sha256_midstate's.
  * If 'true' is returned, '*duplicates' is set according to whether duplicate 'sha256_midstate' values were found.
- * If 'true' is returned, the array is also wiped, with all pointers set to NULL.
- * If malloc fails, false is returned and '*duplicates' and the array are unchanged.
+ * If malloc fails, false is returned and '*duplicates' is unchanged.
  *
  * Precondition: NULL != duplicates;
  *               const sha256_midstate a[len];
- * Postcondition: If 'true' is returned then for all 0 <= i < len, NULL == a[i];
  */
 static inline bool hasDuplicates(bool* duplicates, const sha256_midstate* a, size_t len) {
   _Static_assert(sizeof(a->s) * CHAR_BIT == 256, "sha256_midstate.s has unnamed padding");
