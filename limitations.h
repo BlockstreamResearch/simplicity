984b66ba2df8c8a860af2535818da5ee1730dd87
Russell O'Connor <roconnor@blockstream.io>
2023-03-28 15:37:03 -0400

    Count number of cells
    Count the number of cells the bit machine requires and bound it by a consensus limit.

--- limitations.h
+++ limitations.h
@@ -3,7 +3,9 @@
 
 #define DAG_LEN_MAX 0x800000U
 #define NUMBER_OF_TYPENAMES_MAX 0x1000U
+#define CELLS_MAX 0x500000U /* 5120 Kibibits ought to be enough for anyone. */
 _Static_assert(DAG_LEN_MAX <= SIZE_MAX , "DAG_LEN_MAX doesn't fit in size_t.");
 _Static_assert(NUMBER_OF_TYPENAMES_MAX <= SIZE_MAX, "NUMBER_OF_TYPENAMES_MAX doesn't fit in size_t.");
+_Static_assert(CELLS_MAX <= SIZE_MAX, "CELLS_MAX doesn't fit in size_t.");
 
 #endif
