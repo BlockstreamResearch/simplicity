957b017714d5b9ff17061e6634bfb6acdaf56f02
Christian Lewe <ch_lewe@agdsn.me>
2023-10-16 17:26:20 +0200

    Enable parallel builds for C project
    This will compile the C project on all available CPU cores.

--- Simplicity.C.nix
+++ Simplicity.C.nix
@@ -13,6 +13,7 @@ assert withCoverage -> gcovr != null && gcov-executable != null;
 stdenv.mkDerivation {
   name = "libSimplicity-0.0.0";
   src = lib.sourceFilesBySuffices ./C ["Makefile" ".c" ".h" ".inc"];
+  enableParallelBuilding = true;
   CPPFLAGS = lib.optional (builtins.isString wideMultiply) "-DUSE_FORCE_WIDEMUL_${lib.toUpper wideMultiply}=1";
   CFLAGS = lib.optional withCoverage "--coverage"
         ++ lib.optional production "-DPRODUCTION";
