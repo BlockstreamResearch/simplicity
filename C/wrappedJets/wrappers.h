9d2bda842240c3243faddc91752216cea0db5e99
Christian Lewe <ch_lewe@agdsn.me>
2024-02-06 18:04:57 +0900

    Jet wrapper: Use simplicity asserts

--- C/wrappedJets/wrappers.h
+++ C/wrappedJets/wrappers.h
@@ -1,17 +1,19 @@
 #ifndef WRAPPEDJETS_WRAPPERS_H
 #define WRAPPEDJETS_WRAPPERS_H
 
+#include "simplicity_assert.h"
+
 #define COREWRAP_(jet)                                                                                                        \
 bool c_##jet(frameItem* dst, const frameItem* src) {                                                                          \
   bool result = jet(dst, *src, NULL);                                                                                         \
-  assert (!result || 0 == dst->offset);                                                                                       \
+  simplicity_debug_assert (!result || 0 == dst->offset);                                                                                       \
   return result;                                                                                                              \
 }
 
 #define WRAP_(jet)                                                                                                            \
 bool c_##jet(frameItem* dst, const frameItem* src, const txEnv* env) {                                                        \
   bool result = jet(dst, *src, env);                                                                                          \
-  assert (!result || 0 == dst->offset);                                                                                       \
+  simplicity_debug_assert (!result || 0 == dst->offset);                                                                                       \
   return result;                                                                                                              \
 }
 
