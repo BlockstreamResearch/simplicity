27a1b5d41919282e599cfb9e7e3765494e2da0af
Russell O'Connor <roconnor@blockstream.io>
2024-03-20 17:14:47 -0400

    Per input/output transaction hash jets

--- primitive/elements/checkSigHashAllTx1.h
+++ primitive/elements/checkSigHashAllTx1.h
@@ -9,7 +9,7 @@
  *     Simplicity.Programs.CheckSig.Lib.checkSigVerify' Simplicity.Elements.Programs.SigHash.Lib.sigAllHash
  *     (Simplicity.LibSecp256k1.Spec.PubKey 0x00000000000000000000003b78ce563f89a0ed9414f5aa28ad0d96d6795f9c63)
  *     (Simplicity.LibSecp256k1.Spec.Sig 0x00000000000000000000003b78ce563f89a0ed9414f5aa28ad0d96d6795f9c63
- *                                       0x564a46b432da925100aaa46efd234cbb8cd8d8fff16be82961ed3332b082d8f9)
+ *                                       0xb301d157c25b517ff64c8c88995e99213c9be3c60368367ca01224fdd619261f)
  * with jets.
  */
 extern const unsigned char elementsCheckSigHashAllTx1[];
